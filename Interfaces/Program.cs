2e74ad6b9001f7aa3edd40cd9be480694fa746ab
s3m1h <semih_acar01@hotmail.com>
2022-01-27 16:56:18 +0300

    5.hafta_ödev2_bitti

---
+++ Interfaces/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+
+
+//IPersonManager personManager = new IPersonManager(); // hatalı, referans alamıyorum - newleyemiyorum
+
+IPersonManager customerManager = new CustomerManager(); // başarılı
+IPersonManager employeeManager = new EmployeeManager(); // başarılı
+
+
+
+ProjectManager projectManager = new ProjectManager();
+projectManager.Add(customerManager);
+projectManager.Add(employeeManager);
+projectManager.Update(employeeManager);
+
+
+interface IPersonManager
+{
+    void Add();
+    void Update();
+}
+// inherits - class
+// implements - interface
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Müşteri Eklendi.");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Müşteri Updated.");
+    }
+}
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    { 
+        Console.WriteLine("Personel Eklendi.");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Personel Updated.");
+    }
+
+
+}
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+    public void Update(IPersonManager personManager)
+    {
+        personManager.Update();
+    }
+}
+
+
