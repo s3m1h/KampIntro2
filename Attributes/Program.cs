ac2c410527ee07f64dfe544a3720aca7d2fd4690
s3m1h <semih_acar01@hotmail.com>
2022-03-05 12:42:57 +0300

    .

---
+++ Attributes/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+[ToTable("Customers")]
+[ToTable("Customers")]
+class Customer
+{
+    public int Id { get; set; }
+    [RequiredProperty]
+    public string FirstName { get; set; }
+    [RequiredProperty]
+    public string LastName { get; set; }
+    [RequiredProperty]
+    public int Age { get; set; }
+
+}
+class CustomerDal
+{
+    public void Add(Customer customer) {
+
+        Console.WriteLine(customer.FirstName);
+    }
+}
+
+
+[AttributeUsage(AttributeTargets.Property,AllowMultiple =false)]
+class RequiredPropertyAttribute : Attribute
+{
+
+}
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+class ToTableAttribute: Attribute
+{
+    string _tableName;
+    public ToTableAttribute(string tableName)
+    {
+        _tableName = tableName;
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//DortIslem dortIslem = new DortIslem(5,7);
+//var topla = dortIslem.Topla(2,4);
+//var carp = dortIslem.Carp(5,2);
+//Console.WriteLine(topla);
+//Console.WriteLine(carp);
+
+var type = typeof(DortIslem);
+// DortIslem dortIslem = (DortIslem)Activator.CreateInstance(type,4,3); // calısma anında dorislemi newlemiş oluyoruz.
+// Console.WriteLine(dortIslem.Topla(5, 4));
+// Console.WriteLine(dortIslem.Topla2());
+
+var ornek = Activator.CreateInstance(type, 4, 3);
+
+var result = ornek.GetType().GetMethod("Topla2").Invoke(ornek,null); // bilgi alma 
+Console.WriteLine(result);
+
+class DortIslem
+{
+    int _sayi1, _sayi2;
+    public DortIslem(int sayi1, int sayi2)
+    {
+        _sayi1 = sayi1;
+        _sayi2 = sayi2;
+    }
+    public int Topla(int sayi1, int sayi2)
+    {
+        return sayi1 + sayi2;
+    }
+    public int Carp(int sayi1, int sayi2)
+    {
+        return sayi1 * sayi2;
+    }
+    public int Topla2()
+    {
+        return _sayi1 + _sayi2;
+    }
+    public int Fark()
+    {
+        return _sayi1 - _sayi2;
+    }
+}
